77bd596f3ec19e8e9c7f184e19161d950589137a
Maxime Neveu <neveu.maxime29@gmail.com>
2026-02-21 11:13:42 +0100

    fix error package

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Supprime le bloc eslint s'il est mal placé ou utilise cette syntaxe :
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
